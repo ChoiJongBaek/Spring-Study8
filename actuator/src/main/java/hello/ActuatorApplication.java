d771b30124eedce2719bf5209cb3d95bacff1aa2
whdqor94@naver.com <whdqor94@naver.com>
2024-02-27 22:45:07 +0900

    2024.02.27 매트릭 카운터 등록

--- actuator/src/main/java/hello/ActuatorApplication.java
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,11 +1,13 @@
 package hello;
 
 import hello.order.v0.OrderConfigV0;
+import hello.order.v1.OrderConfigV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderConfigV0.class)
+//@Import(OrderConfigV0.class)
+@Import(OrderConfigV1.class)
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
